3b28e5bd05a65a963b62eb5cfddd149168a177fb
Muammar81 <Muammar.Yacoob@gmail.com>
2022-10-21 13:45:35 +0100

    Player Color syncs well

--- Assets/Scripts/PlayerColor.cs
+++ Assets/Scripts/PlayerColor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Fusion;
 using UnityEngine;
@@ -8,17 +9,33 @@ public class PlayerColor : NetworkBehaviour
     [SerializeField] private Color[] playerColors;
     private Material mat;
 
-    //[Networked(OnChanged = nameof(OnColorChanged))]
-    public int colorIndex { get; set; }
+    [Networked(OnChanged = nameof(OnColorChanged))]
+    int colorIndex { get; set; }
+
+    private void Awake()=> mat = GetComponentInChildren<Renderer>().material;
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            //data.colorButton;
+        }
+    }
 
     private void Start()
     {
-        mat = GetComponentInChildren<Renderer>().material;
-        colorIndex = Runner.ActivePlayers.Count() - 1;
-        mat.color = playerColors[colorIndex];
+        colorIndex = Runner.ActivePlayers.Count() - 1; //Trigers Property OnChange
+        //Sets color locally
+        SetColor();
     }
 
-    private void OnColorChanged()
+    public static void OnColorChanged(Changed<PlayerColor> changed)
+    {
+        changed.LoadNew();
+        changed.Behaviour.SetColor();
+    }
+
+    private void SetColor()
     {
         mat.color = playerColors[colorIndex];
     }
